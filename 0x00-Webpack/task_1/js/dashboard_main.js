1e2e8c0bbfabb7f1ea46d1d0c032988ebc844920
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-29 18:15:29 +0100

    fixing task1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,16 +2,16 @@ import $ from "jquery";
 import _ from 'lodash';
 
 
+let count = 0;
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
 function updateCounter() {
-    $('#count').html(count + 'clicks on the button');
     count += 1;
+    $('#count').html(count + 'clicks on the button');
 }
 const debounced = _.debounce(updateCounter, 1000);
 $('button').on('click', debounced);
