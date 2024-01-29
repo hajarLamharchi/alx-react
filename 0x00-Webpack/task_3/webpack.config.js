0b4ea6e575fadc1d8c417632d10222c265bb1eec
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-29 22:24:56 +0100

    task3

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        header: './modules/header/header.js',
+        body: './modules/body/body.js',
+        footer: './modules/footer/footer.js',
+    }, 
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].bundle.js'
+    },
+    performance: {
+        hints: false,
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new CleanWebpackPlugin()
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    devServer: {
+        port: 8564,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
