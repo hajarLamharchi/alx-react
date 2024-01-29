487bb38a5da952111ea7a5c075484d81872972e6
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-29 17:00:56 +0100

    task0

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,9 +1,5 @@
 import $ from "jquery";
 
-const firstParagraph = $('<p>Holberton Dashboard</p>');
-const secondParagraph = $('<p>Dashboard data for the students</p>');
-const thirdParagraph = $('<p>Copyright - Holberton School</p>');
-
-$('body').append(firstParagraph);
-$('body').append(secondParagraph);
-$('body').append(thirdParagraph);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
