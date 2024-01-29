e34762f5e106db4c6785d242b17d938b36d8a9ad
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-29 15:49:39 +0100

    fixing index.js

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,11 +1,9 @@
 import $ from 'jquery';
 
-$(document).ready(function() {
-    const firstParagraph = $('<p>Holberton Dashboard</p>');
-    const secondParagraph = $('<p>Dashboard data for the students</p>');
-    const thirdParagraph = $('<p>Copyright - Holberton School</p>');
+const firstParagraph = $('<p>Holberton Dashboard</p>');
+const secondParagraph = $('<p>Dashboard data for the students</p>');
+const thirdParagraph = $('<p>Copyright - Holberton School</p>');
 
-    $('body').append(firstParagraph);
-    $('body').append(secondParagraph);
-    $('body').append(thirdParagraph);
-});
+$('body').append(firstParagraph);
+$('body').append(secondParagraph);
+$('body').append(thirdParagraph);
