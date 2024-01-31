06aaf9d094f9028b259764177996f7abdd0d5281
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-31 15:10:50 +0100

    fixing errors

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -5,23 +5,28 @@ import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
       return (
-        <div className="App">
+       <div className="App">
           <div className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1>School dashboard</h1>
           </div>
           <div className="App-body"> 
             <p>Login to access the full dashboard</p>
-            <label for="email">Email:</label>
+            <label htmlFor="email">Email:</label>
             <input type="text" id="email" name="email"></input>
-            <label for="password">Password:</label>
+            <label htmlFor="password">Password:</label>
             <input type="password" id="password" name="password"></input>
-            <button>OK</button>
+            <button style={{
+                  background: "transparent",
+                  borderRadius: '5px',
+                  margin: "0 0 0 10px",
+                  border: "1px solid gray"
+            }}>OK</button>
           </div>
           <div className="App-footer">
             <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
           </div>
-        </div>
+        </div>  
       )
     }
   
