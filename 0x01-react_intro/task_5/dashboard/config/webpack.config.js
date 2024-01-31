e073851509db64b5f1d5001aedab487eebd6264e
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-31 18:43:28 +0100

    task9

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js')
+    }, 
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        hot: true,
+        static: path.join(__dirname, './public'),
+        compress: true,
+        port: 8564,
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    devtool: 'inline-source-map',
+    performance: {
+        hints: false,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
