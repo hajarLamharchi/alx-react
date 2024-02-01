df5e75897b4319c71660bf72b7ff3fc397854807
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-02-01 20:03:58 +0100

    writing tests for each component

---
+++ 0x02-react_props/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Notifications from './Notifications';
+import { shallow } from 'enzyme';
+
+
+describe('<Header />', () => {
+    it('Notifications renders without crashing', () => {
+        const wrapper = shallow(<Notifications />);
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    it('Notifications renders the text Here is the list of notifications', () => {
+        const wrapper = shallow(<Notifications />);
+        expect(wrapper.text()).toContain('Here is the list of notifications');
+    });
+});
