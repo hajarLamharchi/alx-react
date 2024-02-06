5e627367def09a0b6374686f48b7570d12459863
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-02-06 23:35:48 +0100

    task0

---
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import "./App.css";
+import Notifications from "../Notifications/Notifications";
+import PropTypes from "prop-types";
+import CourseList from "../CourseList/CourseList";
+import { getLatestNotification } from "../utils/utils";
+
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: getLatestNotification() },
+];
+
+class App extends Component {
+  static propTypes = {
+    isLoggedIn: PropTypes.bool,
+  };
+  static defaultProps = {
+    isLoggedIn: true,
+  };
+  
+  render() {
+    const { isLoggedIn } = this.props;
+    return (
+      <div className="App">
+        <Notifications listNotifications={ listNotifications } />
+        <Header />
+        {isLoggedIn ? <CourseList listCourses={ listCourses } /> : <Login />}
+        <Footer />
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
