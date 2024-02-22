014fdf6cbcfda428bc32418fb7bbddf7dcde0394
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-02-22 16:23:53 +0100

    lazy seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,7 +1,7 @@
-import { Seq } from 'immutable';
+import { Seq, fromJS } from 'immutable';
 
 const printBestStudents = (object) => {
-    const lazySeq = Seq(object)
+    const lazySeq = Seq(fromJS(object))
     .filter((student) => student.get('score') >= 70)
     .map((student) => student.update('firstName', firstName => firstName.charAt(0).toUpperCase() + firstName.slice(1))
     .update('lastName', lastName => lastName.charAt(0).toUpperCase() + lastName.slice(1)));
