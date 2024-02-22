d94ccab9c46c0cc7a0e0e4e740b7d51fd106eac1
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-02-22 16:11:06 +0100

    lazy seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -7,7 +7,7 @@ const printBestStudents = (object) => {
     .map((student) => student.update('firstName', firstName => firstName.charAt(0).toUpperCase() + firstName.slice(1))
     .update('lastName', lastName => lastName.charAt(0).toUpperCase() + lastName.slice(1)));
 
-    console.log(lazySeq);
+    console.log(lazySeq.toJS());
 }
 
 export default printBestStudents;
